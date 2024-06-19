0d2eb857e66ea309f1327b41000a1f77d3f38e48
makkentoshi <pochta.zdraste@mail.ru>
2024-06-19 12:43:44 +0500

    "Updated HomeCard component to use title and description props instead of hardcoded text"

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -27,8 +27,8 @@ const HomeCard = ({ className, img, title, description, handleClick }: HomeCardP
         />
       </div>
       <div className="flex flex-col gap-2">
-        <h1 className="text-2xl font-bold">New Meeting</h1>
-        <p className="text-lg font-normal">Start a new meeting</p>
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <p className="text-lg font-normal">{description}</p>
       </div>
     </div>
   );
