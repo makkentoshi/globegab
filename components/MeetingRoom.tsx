79fbc57b175bfe6078b57f87794b1f81b9903c30
makkentoshi <pochta.zdraste@mail.ru>
2024-06-19 18:09:48 +0500

    feat: Update MeetingRoom component with dynamic layout and participant list

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import { cn } from "@/lib/utils";
+import {
+  CallParticipantsList,
+  PaginatedGridLayout,
+  SpeakerLayout,
+} from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
+
+type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [layout, setLayout] = useState("speaker-left");
 
-export default MeetingRoom
+  const [showParticipants, setShowParticipants] = useState(false);
+
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout></PaginatedGridLayout>;
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition="left"></SpeakerLayout>;
+
+      default:
+      case "speaker-left":
+        return <SpeakerLayout participantsBarPosition="right"></SpeakerLayout>;
+    }
+  };
+  return (
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items-center">
+          <CallLayout></CallLayout>
+        </div>
+        <div
+          className={cn("h-[calc(100vh-86px)] ml-2 hidden", {
+            "show-block": showParticipants,
+          })}
+        >
+          <CallParticipantsList
+            onClose={() => setShowParticipants(false)}
+          ></CallParticipantsList>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MeetingRoom;
